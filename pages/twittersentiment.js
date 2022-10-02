86cb78898cf96b15e48a2190c929fbdfee51c2af
Audrey Wang <audreywang2006@gmail.com>
2022-10-02 12:08:15 -0700

    checkbox filter feed

--- pages/twittersentiment.js
+++ pages/twittersentiment.js
@@ -2,6 +2,7 @@ import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Sentiment from "sentiment";
 import displayUserTweets from "./api/twitter";
+import Nav from "../components/nav";
 
 export default function TwitterSentiment({ tweets, home }) {
   const { data: session } = useSession();
@@ -26,65 +27,181 @@ export default function TwitterSentiment({ tweets, home }) {
       red_result.push(tweet.text);
     }
   }
+
+  const [checkedGr, setCheckedGr] = React.useState(false);
+  const [checkedYe, setCheckedYe] = React.useState(false);
+  const [checkedRe, setCheckedRe] = React.useState(false);
+
+  function handleGreenCheck() {
+    setCheckedGr(!checkedGr);
+  }
+  function handleYellowCheck() {
+    setCheckedYe(!checkedYe);
+  }
+  function handleRedCheck() {
+    setCheckedRe(!checkedRe);
+  }
   return (
     <div>
       {session ? (
         <div>
-          <h3>Signed in as {session.user.name}</h3>
-          <br />
-          {green_result.map((i) => (
-            <div>
-              <h2
-                style={{
-                  borderColor: "green",
-                  borderRadius: "10px",
-                  borderStyle: "solid",
-                  borderWidth: "medium",
-                }}
-              >
+          <Nav />
+
+          <ul className="ml-5 w-48 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+            <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                <input
+                  id="green-checkbox"
+                  type="checkbox"
+                  onClick={handleGreenCheck}
+                  className="w-4 h-4 text-green-600 bg-gray-100 rounded border-gray-300 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="green-checkbox"
+                  className="py-3 ml-2 text-sm font-medium text-green-800 dark:text-green-300"
+                >
+                  Positive
+                </label>
+              </div>
+            </li>
+            <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                <input
+                  id="yellow-checkbox"
+                  type="checkbox"
+                  onClick={handleYellowCheck}
+                  className="w-4 h-4 text-yellow-400 bg-gray-100 rounded border-gray-300 focus:ring-yellow-300 dark:focus:ring-yellow-400 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="yellow-checkbox"
+                  className="py-3 ml-2 text-sm font-medium text-yellow-500 dark:text-yellow-100"
+                >
+                  Neutral
+                </label>
+              </div>
+            </li>
+            <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                <input
+                  id="red-checkbox"
+                  type="checkbox"
+                  onClick={handleRedCheck}
+                  className="w-4 h-4 text-red-600 bg-gray-100 rounded border-gray-300 focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="red-checkbox"
+                  className="py-3 ml-2 text-sm font-medium text-red-800 dark:text-red-200"
+                >
+                  Negative
+                </label>
+              </div>
+            </li>
+          </ul>
+
+          {/* {green_result.map((i) => (
+            <div className="mx-96">
+              <h2 className="border-green-800 p-2 rounded-lg border-solid border-4">
                 {i}
               </h2>
               <br />
             </div>
           ))}
           {yellow_result.map((i) => (
-            <div>
-              <h2
-                style={{
-                  borderColor: "yellow",
-                  borderRadius: "10px",
-                  borderStyle: "solid",
-                  borderWidth: "medium",
-                }}
-              >
+            <div className="mx-96">
+              <h2 className="border-yellow-400 p-2 rounded-lg border-solid border-4">
                 {i}
               </h2>
               <br />
             </div>
           ))}
           {red_result.map((i) => (
-            <div>
-              <h2
-                style={{
-                  borderColor: "red",
-                  borderRadius: "10px",
-                  borderStyle: "solid",
-                  borderWidth: "medium",
-                }}
-              >
+            <div className="mx-96">
+              <h2 className="border-red-600 p-2 rounded-lg border-solid border-4">
                 {i}
               </h2>
               <br />
             </div>
-          ))}
-          <br />
-          <button onClick={() => signOut()}>Sign out</button>
+          ))} */}
+          {checkedGr ? (
+            <div className="p-5 mb-4 mx-96 bg-green-100 rounded-lg border border-green-800 dark:bg-green-600 dark:border-green-50">
+              <h1 className="text-lg font-semibold text-green-800 dark:text-white">
+                Positive
+              </h1>
+              <ol className="mt-3 divide-y divider-gray-200 dark:divide-gray-700">
+                {green_result.map((i) => (
+                  <li>
+                    <a
+                      href="#"
+                      className="block items-center p-3 rounded-lg sm:flex hover:bg-green-200 dark:hover:bg-green-700"
+                    >
+                      <div className="text-gray-800 dark:text-gray-100">
+                        <div className="text-base font-normal">
+                          <h2>{i}</h2>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {checkedYe ? (
+            <div className="p-5 mb-4 mx-96 bg-yellow-100 rounded-lg border border-yellow-400 dark:bg-yellow-400 dark:border-yellow-50">
+              <h1 className="text-lg font-semibold text-yellow-400 dark:text-white">
+                Neutral
+              </h1>
+              <ol className="mt-3 divide-y divider-gray-200 dark:divide-gray-700">
+                {yellow_result.map((i) => (
+                  <li>
+                    <a
+                      href="#"
+                      className="block items-center p-3 rounded-lg sm:flex hover:bg-yellow-200 dark:hover:bg-yellow-600"
+                    >
+                      <div className="text-gray-800 dark:text-gray-100">
+                        <div className="text-base font-normal">
+                          <h2>{i}</h2>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          ) : (
+            <div></div>
+          )}
+
+          {checkedRe ? (
+            <div className="p-5 mb-4 mx-96 bg-red-100 rounded-lg border border-red-600 dark:bg-red-600 dark:border-red-50">
+              <h1 className="text-lg font-semibold text-red-600 dark:text-white">
+                Negative
+              </h1>
+              <ol className="mt-3 divide-y divider-gray-200 dark:divide-gray-700">
+                {yellow_result.map((i) => (
+                  <li>
+                    <a
+                      href="#"
+                      className="block items-center p-3 rounded-lg sm:flex hover:bg-red-200 dark:hover:bg-red-700"
+                    >
+                      <div className="text-gray-800 dark:text-gray-100">
+                        <div className="text-base font-normal">
+                          <h2>{i}</h2>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
       ) : (
         <div>
-          <h3>Not signed in</h3>
-          <br />
-          <button onClick={() => signIn()}>Sign in</button>
+          <Nav />
         </div>
       )}
     </div>
